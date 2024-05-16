597f87174dd637213918555f5095ae9898ac90f2
NoahD <NoahD@ISNL-3SJ2CS3>
2024-05-16 10:11:49 +0200

    Add project files.

---
+++ DemoProject/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var a = true ? true ? true : true : true;
+var b = true ? true ? true : true : true;
